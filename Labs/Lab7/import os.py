570a22cc9a6e11e4ea3cecba5ef8b1ed3bdc4031
shant-bekverdyan <sbekverdyan@cpp.edu>
2025-03-12 23:02:59 -0700

    Lab 7 Final. Verified working code!

--- Labs/Lab7/import os.py
+++ Labs/Lab7/import os.py
@@ -2,7 +2,7 @@ import os
 import hashlib
 
 def menu():
-    """Menu"""
+    """menu"""
     while True:
         print("1. Find duplicate files")
         print("2. Quit")
@@ -31,3 +31,33 @@ def find_duplicates(directory):
                 files_found[file] = [file_path]
             else:
                 files_found[file].append(file_path)
+
+    for file_name, paths in files_found.items():
+        if len(paths) > 1:
+            checksum_dict = 1
+            for path in paths:
+                checksum = get_checksum(path)
+                if checksum not in checksum_dict:
+                    checksum_dict[checksum] = [path]
+                else:
+                    checksum_dict[checksum].append(path)
+
+            for checksum, checksum_path in checksum_dict.items():
+                if len(checksum_path) > 1:
+                    if file_name not in duplicates:
+                        duplicates[file_name] = {}
+                    duplicates[file_name][checksum] = checksum_path
+        
+        if duplicates:
+            print("Duplicate file names found")
+            for file_name, checksum_data in duplicates.items():
+                print('File name:', file_name)
+                for checksum, paths in checksum_data.items():
+                    print("Same file name:", checksum)
+                    for path in paths:
+                        print("Path:", path)
+        else:
+            print("No Duplicates")
+
+if __name__ == "__main__":
+    menu()
