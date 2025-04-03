d35e9d8d7762e8a13aa068b39788d244d47eaaa9
Shant Bekverdyan <sbekverdyan@cpp.edu>
2025-04-02 21:03:55 -0700

    prime number check

---
+++ Project 2/Project2.py
@@ -0,0 +1,20 @@
+import time
+import math
+
+def is_prime(number):
+    
+    """checks to see if a number is prime or not"""
+    
+    if number <= 1:
+        return False 
+    if number <= 3:
+        return True   
+    if number % 2 == 0 or number % 3 == 0:
+        return False  
+
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i += 6
+    return True
